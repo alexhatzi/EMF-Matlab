e3eaf4bccee8b5cda5d95f41b0719b4d60c43a45
alex hatzi <alexhatz005@gmail.com>
2023-10-10 20:49:07 -0400

    im cookin now

--- Homework-6/Problem9.m
+++ Homework-6/Problem9.m
@@ -1,4 +1,10 @@
-%Equation 4.33
+%Equation 4.115 neglecting length of capaictor because we only care about
+%observation POINT
+
+%Also youre just supposed to "know" somehow i guess but you basically treat
+%the voltage from a to b in this case as the observation point to the
+%origin so that makes it a lot easier
+
 
 clc
 clear
@@ -10,33 +16,21 @@ bleh = 1/(2*pi*eps) ; %   just one over 2 pi epsilon not
 
 p1 = [5 0 0] ;       %Location of first infinite line charge (technically z could be another variable)
 p2 = [-5 0 0] ;      % Second line
-P  = [3 4 0] ;       %Observation point
+po  = [3 4 0] ;       %Observation point
 
-r1 = P-p1 ;          %Distance from Obsveration point to 1st line as vector
-R1 = norm(r1) ;      %Actual distance
-    
-r2 = P-p2 ;          %same for second line
-R2 = norm(r2) ;
+P1 = norm(p1) ; %Will need to calculate the radius and shiii 
+P2 = norm(p2) ; 
 
+d1 = po - p1 ;  %Distance form observation point to line charges
+d2 = po - p2 ; 
 
-rhat1 = r1/R1 ;      % Calculating unit vector (rhat in formula)
-rhat2 = r2/R2 ;
+D1 = norm(d1) ; 
+D2 = norm(d2) ; 
 
-
-E1 = bleh* (1/R1) ;       %Using equation 4.33 from textbook to calculate Electric Field
-E2 = bleh* (1/R2) ;
+step3 = bleh*log(P1/D1) ; 
+step4 = -1*bleh*log(P2/D2) ; 
 
 
-%Calculating E dot dl from equation 4.39
+step5 = step3 + step4 ; 
+vpa(step5) 
 
-
-
-%Now integrating
-%P1 is distance from line to origin
-%P2 is distance from line to observation point with respect to the fact
-
-step3 = int(E1,r,R1,5) ;
-step4 = int(-E2,r,R2,-5) ;
-
-step5 = step4 + step3 ; 
-vpa(step5)
