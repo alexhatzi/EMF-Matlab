02f9f0844088e7250228dc59139a87863a0144f3
alex hatzi <alexhatz005@gmail.com>
2023-10-24 20:48:52 -0400

    i cant take this anymore

--- Homework8/Problem4.m
+++ Homework8/Problem4.m
@@ -8,9 +8,45 @@ u0 = 4*pi*10^(-7) ;
 u1 = 4*u0 ; 
 u2 = 6*u0 ; 
 
+yhat = [0 1 0] ;
+zhat = [0 0 1] ;
+
 plane = y == 1 - z;
 
-H1 = u1*B1 ;
-H1Tangent = sqrt( H1(1)^2 + H1(3)^2) ; 
-step1 = atan(H1Tangent/H1(2)) ;
+%First step is to find the normal vector of the plane (Does not rely on
+%anything other than the plane's equation!!!!
+
+% normal = ahat cos(θ) + bhat cos(θ)
+
+%First find θ (angle of normal vector off (normally x in this case y axis)
+%Pick any two points in this case (0,1) and (1,0) and arctan
+
+step1 = atan(1/1)  ; 
+%Now to find normal vector, use equation from line 16
+step2 = yhat*cos(step1) + zhat*sin(step1) ;
+%Now do dot product with known vector to see how much they align
+step3 = dot(step2,B1) ; % Paralellism of the two vectors
+step4 = step3 * step2 ; % <-- This is our normal component (parallelism * normal vector of plane)
+
+% B1t = B1 - B1n
+
+B1t = B1 - step4 ; 
+
+% B1n = B2n 
+B2n = step4 ;
+
+%B1t/u1 = B2t/u2  
+% ==>   |
+%       V
+
+B2t = (u2/u1) * B1t  ;
+
+B2 = B2n + B2t 
+
+
+
+
+
+
+
 
