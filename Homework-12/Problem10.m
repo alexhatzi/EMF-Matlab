36a25e9e6d05b48af7fcfa9261b5341551628a13
alex hatzi <alexhatz005@gmail.com>
2023-12-04 16:44:57 -0500

    problem 10 is just conceptual

---
+++ Homework-12/Problem10.m
@@ -0,0 +1,10 @@
+clc
+clear
+
+% a.) a1 = a2  Therefore --> Circular
+%     +X cross +Y = +Z, then using right hand rule of +Z, turns left
+
+% b.) a1 = a2 Therefore circular
+%      +X cross -Y = -Z, turns right
+
+% c.) % Not circular and not linear
